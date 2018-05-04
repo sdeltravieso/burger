e2f301e6cde16dcb3be080c7ddbdff58ed9f11cd
Spencer Daniels <speencah@spencer-danielss-macbook-pro.local>
2018-05-04 00:09:05 -0600

    final push

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole Bacon Burger', false);
